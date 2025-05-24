185d1320cc37843079f2d7a031a10e895e955fbd
Onyinyechi Onuorah <janeonuorah2000@gmail.com>
2025-05-24 23:00:10 +0100

    a script that prints a square

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+let squareSize = parseInt(process.argv[2]);
+
+if (isNaN(squareSize)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < squareSize; i++) {
+    console.log("X".repeat(squareSize));
+  }
+}
