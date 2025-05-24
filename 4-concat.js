41d7ae071e8e47ff6c1ae63d9280b0af060f4f65
Onyinyechi Onuorah <janeonuorah2000@gmail.com>
2025-05-24 18:04:34 +0100

    a script that prints two arguments passed to it, in the following format: “ is ”

--- 4-concat.js
+++ 4-concat.js
@@ -3,7 +3,7 @@ let firstArg = args[0];
 let secondArg = args[1];
 
 if (args.length === 0) {
-  console.log("No arguments");
+  console.log("0 argument");
 } else if (args.lenght === 1) {
   console.log(firstArg + " is ");
 } else {
